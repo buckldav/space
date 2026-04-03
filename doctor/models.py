d4438224d1886f35f7e0320e36697145a0188483
WBriggy <Brigham.Lawrence@meritknights.com>
2026-04-03 12:22:44 -0600

    make doctors and medical tasks

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class IllCondition(models.Model):
+	name=models.CharField(max_length=100)
+	description=models.TextField()
+	isCompleted=models.BooleanField(default=False)
+	severity=models.IntegerField()
