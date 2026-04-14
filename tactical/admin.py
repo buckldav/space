22e5a2f53def7cd3391b4730f745cd40cdca432c
David Wiseman <Dwiseman308@gmail.com>
2026-04-14 13:18:13 -0600

    stufff

--- tactical/admin.py
+++ tactical/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from tactical import models
 
 # Register your models here.
-random_value_that_is_never_used_the_fifth = "hi!"
+random_value_that_is_never_used_the_fifth = "hi!" # Ignore this, program might break without : )
+admin.site.register(models.Phasers)
