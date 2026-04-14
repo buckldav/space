9357c57f58e4144fa981db200400370d592ef497
David Buckley <dbuckley@springmicro.com>
2026-04-14 13:14:52 -0600

    base style

--- config/urls.py
+++ config/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     # ...
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
 
 # API URLS
