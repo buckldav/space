5be0d749d2623be967033349baa7015a6aec8109
David Wiseman <Dwiseman308@gmail.com>
2026-04-21 12:55:32 -0600

    Phasers Be a Phasin

--- tactical/admin.py
+++ tactical/admin.py
@@ -3,4 +3,10 @@ from tactical import models
 
 # Register your models here.
 random_value_that_is_never_used_the_fifth = "hi!" # Ignore this, program might break without : )
-admin.site.register(models.Phasers)
+
+class PhasersAdmin(admin.ModelAdmin):
+    list_display = ['phaser_is_working', 'direction_fireing']
+    list_filter = ['phaser_is_working', 'direction_fireing']
+
+
+admin.site.register(models.Phasers, PhasersAdmin)
