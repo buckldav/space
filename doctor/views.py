e02cef53de7869812f23d098a7774a739d666946
WBriggy <Brigham.Lawrence@meritknights.com>
2026-05-14 13:15:10 -0600

    fixed pothole

--- doctor/views.py
+++ doctor/views.py
@@ -7,8 +7,12 @@ from django import forms
 # Create your views here.
 def condition_list(request):
 	conditions = IllCondition.objects.all()
+	malaria = IllCondition.objects.filter(name="Malaria").first()
+	malaria_severity = malaria.severity if malaria else 0
+		
+
 	conditions = sorted(conditions, key=lambda c: c.severity, reverse=True)
 	
-	return render(request, "conditions/index.html", {"conditions": conditions, "infected": (random.randint(0, 100)*random.randint(0, 100)/100)})#randomNumber is a placeholder, replace with actual value when available
+	return render(request, "conditions/index.html", {"conditions": conditions, "infected": malaria_severity})#randomNumber is a placeholder, replace with actual value when available
 
 
